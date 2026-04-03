0fdde1a56291f28bfe1ebd377121f48138e6ac7f
Rain Zhang <tza103@sfu.ca>
2026-04-03 03:22:07 -0700

    Read modal scroll lock from shared state

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,16 @@
 // Modal scroll lock functions
 
 function applyModalScrollLock() {
-    if (modalScrollLockState.locked) {
+    const scrollLockState = PORTFOLIO_STATE.modal.scrollLockState
+
+    if (scrollLockState.locked) {
         return
     }
 
     const rootElement = document.documentElement
     const scrollbarWidth = Math.max(0, window.innerWidth - rootElement.clientWidth)
 
-    modalScrollLockState.previousPaddingRight = rootElement.style.paddingRight
+    scrollLockState.previousPaddingRight = rootElement.style.paddingRight
 
     if (scrollbarWidth > 0) {
         rootElement.style.paddingRight = `${scrollbarWidth}px`
@@ -17,17 +19,18 @@ function applyModalScrollLock() {
     rootElement.classList.add("modal-open")
     document.body.classList.add("modal-open")
 
-    modalScrollLockState.locked = true
+    scrollLockState.locked = true
 }
 
 function releaseModalScrollLock() {
+    const scrollLockState = PORTFOLIO_STATE.modal.scrollLockState
     const rootElement = document.documentElement
 
     rootElement.classList.remove("modal-open")
     document.body.classList.remove("modal-open")
 
-    rootElement.style.paddingRight = modalScrollLockState.previousPaddingRight
+    rootElement.style.paddingRight = scrollLockState.previousPaddingRight
 
-    modalScrollLockState.locked = false
-    modalScrollLockState.previousPaddingRight = ""
+    scrollLockState.locked = false
+    scrollLockState.previousPaddingRight = ""
 }
